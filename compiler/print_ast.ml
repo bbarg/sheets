075851084844bc337971e99e9a25b2c15230af28
arb2196 <arb2196@columbia.edu>
2014-12-12 17:42:11 -0500

    Added printing sdecls

--- compiler/print_ast.ml
+++ compiler/print_ast.ml
@@ -1,15 +1,24 @@
 open Scanner;;
 open Parser;;
+open Printf;;
 
 let print_vdecls vdecls = 
   let f (_type, name, isConst, isStruct) =
     Printf.printf "%s, %s\n" _type name
   in List.map f vdecls
 ;;
-	   
+
+let print_sdecls sdecls =
+    let f (s_name, s_elements) =
+        printf "%s: <" s_name;
+        print_vdecls s_elements;
+        printf(">\n");
+    in List.map f sdecls
+;;
+
 let print_everything (vdecls, sdecls, fdecls) =
-  print_vdecls vdecls
-  (* print_decl_list sdecls; *)
+    print_vdecls vdecls;
+    print_sdecls sdecls
   (* print_decl_list fdecls; *)
 ;;
 

ef3a9acbb41df949ce727c4360b6c7d9550db9db
Benjamin Barg <benbakerbarg@gmail.com>
2014-12-13 02:44:11 -0500

    global variable declarations can now be generated

--- compiler/generator.ml
+++ compiler/generator.ml
@@ -9,7 +9,10 @@
   
   Conventions: 
   - generation functionality will result in a series of
-    strings that can be printed to a file or printed to stdout *)
+    strings that can be printed to a file or printed to stdout
+  - scoped symbol tables will be implemented with a list of
+    Map.Make(String) (we may need several maps for variables,
+    structs, etc *)
 
 open Parser;;
 open Scanner;;
@@ -17,30 +20,31 @@ open Ast;;
 open Printf;;  
 
 module SymbolTable = Map.Make(String);;
-type entry = Ast.vdecl | Ast.sdef | Ast.fdecl;;
 
-exception DuplicateNameError of Ast.vdecl
+exception DuplicateNameError of Ast.vdecl;;
+exception SyntaxError of int * int * string;;  
 
 (* generate the C representation of an individual vdecl *)
 let c_vdecl vdecl =
   let prefix vdecl = match (vdecl.isConst, vdecl.isStruct) with
-    (true, true)  -> "const struct"
-  | (true, false) -> "const"
-  | (false, true) -> "struct"
+    (true, true)  -> "const struct "
+  | (true, false) -> "const "
+  | (false, true) -> "struct "
   | _             -> ""
   in
-  (prefix vdecl) ^ " " ^ vdecl.v_type ^ " " ^ vdecl.v_name ^ ";\n"
+  (prefix vdecl) ^ vdecl.v_type ^ " " ^ vdecl.v_name ^ ";\n"
 ;;							       
 
-				  
 (* return updated table and a generated C string of the var-decs *)
 let gen_global_vdecls (vdecls, sdefs, fdecls) table =
   let validate_vdecl (table, text) vdecl =
-    try SymbolTable.find vdecl.v_name table
+    try
+      SymbolTable.find vdecl.v_name table;
+      raise (DuplicateNameError(vdecl))
     with Not_found ->
       ((SymbolTable.add vdecl.v_name vdecl table), text ^ (c_vdecl vdecl));
-      raise (DuplicateNameError(vdecl))
-  and let text = "" in       
+  in
+  let text = "" in       
   List.fold_left validate_vdecl (table, text) (List.rev vdecls)
 ;;
   
@@ -56,6 +60,6 @@ let _ =
         raise (SyntaxError (line, col, tok))
   in
   let table = SymbolTable.empty in
-  let table, global_vdecls = gen_global_vdecls program table in
-  print_string global_vdecls;
+  let table, c_vdecls_text = gen_global_vdecls program table in
+  print_string c_vdecls_text
 ;;
--- compiler/test.sh
+++ compiler/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./preprocessor.py `pwd`/$1.sht
-cat $1.proc.sht | ./print_ast
+cat $1.proc.sht | ./generator

f211477e3e29d91e6b64d0d8723c366eed879541
arb2196 <arb2196@columbia.edu>
2014-12-14 17:38:56 -0500

    test changes

--- compiler/environment_tests/test_env.sh
+++ compiler/environment_tests/test_env.sh
@@ -2,7 +2,7 @@
 echo "-----------------------------------"
 echo "Running Test:" $1
 echo "-"
-head -n 1 $1.sht| grep "#~"  | sed "s/#~ //" | sed "s/~#//"
+head -n 1 $1.sht| grep "#"  | sed "s/#~ //" | sed "s/~#//" 
 
 
 echo "-----------------------------------"

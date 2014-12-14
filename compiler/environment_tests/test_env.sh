836166c4afaf5906f00cc4b7cda0bed382b462c4
Gabriel Blanco <blancgab@gmail.com>
2014-12-14 12:11:04 -0500

    for Ruch

--- compiler/environment_tests/test_env.sh
+++ compiler/environment_tests/test_env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "-----------------------------------"
 echo "Running Test:" $1
+echo "-"
+
+
 echo "-----------------------------------"
 ../preprocessor.py  `pwd`/$1.sht
 cat $1.proc.sht | ../generator

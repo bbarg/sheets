dad579ad2e4da814ba7b1f098345c86c5ae78c5a
Benjamin Barg <benbakerbarg@gmail.com>
2014-12-14 17:48:27 +0000

    removed opencl libs, re-added platform header

---
+++ opencl/platforms/include/aws-g2.2xlarge.h
@@ -0,0 +1,13 @@
+/* aws-g2.2xlarge.h
+   
+   #defines for AWS Amazon Linux instance with Nvidia GRID K520 driver
+ */
+
+#ifndef _AWS_G2_2XLARGE_H_
+#define _AWS_G2_2XLARGE_H_
+
+#define SHEETS_PLAT_NAME           "NVIDIA Corporation"
+#define SHEETS_DEV_NAME            "GRID K520"
+#define SHEETS_KERNEL_COMPILE_OPTS "-cl-std=CL1.1"
+
+#endif

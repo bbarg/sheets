2d0810f5193d5116bda0c7fb30ed244686c71bb7
Benjamin Barg <benbakerbarg@gmail.com>
2014-12-17 20:19:07 -0500

    cleaned environment and generator_utilities

--- compiler/environment.ml
+++ compiler/environment.ml
@@ -1,22 +1,9 @@
-(* environment.ml
-
-   Scoped symbol table implementation for Sheets. We have three symbol
-   tables:
-   - variable names to vdecl objs
-   - function names to fdecl objs
-
-   Scoping will be implemented by a linked list to which we add when
-   we go down in scope.
-
-   Notes:
-
-   - There should only ever be one entry per name in the symbol table *)
+(* environment.ml *)
 
 open Ast;;
 
 module VariableMap = Map.Make(String);;
 module FunctionMap = Map.Make(String);;
-(* module StructMap   = Map.Make(String);; *) 
 
 exception EmptyEnvironmentError;;
 exception NameAlreadyBoundError of string;;
@@ -34,8 +21,6 @@ type func_info  = {
 	arg_names: string list;
 	_blocksize : int;
 }
-(* Create a record type for env 
- *)
 
 (* Record indicating what the current environment keeps track of *)
 type env = {
@@ -49,61 +34,52 @@ type env = {
     num_array_returns : int;
     var_array_sizes : int VariableMap.t; 
   } 
-(* Types that can be returned by the generator as it modifies 
- * either the text of the generated code 
- * changes the environment as it is parsing the file 
- * or passes fuunctions to edit both code and environment 
- * either in the existing scope or in a new scope 
- *) 
+
+(* Types that can be returned by the generator as it modifies either
+   the text of the generated code changes the environment as it is
+   parsing the file or passes fuunctions to edit both code and
+   environment either in the existing scope or in a new scope *)
 type source = 
   | Text of string
   | Env of (env -> env) 
   | Generator of (env -> (string * env))
   | NewScope of (env -> (string * env))
 
-
 (* Create initializes an empty record for environment *)
 let add_default_func f_id fmap = 
-    let f_info = 
-        { 
-            id = f_id; 
-            gpu = false;
-            return = Int; 
-            args = [];
-            arg_names = [];
-            _blocksize = -1; 
-       }
-    in 
-    (FunctionMap.add f_id f_info fmap)
-
+  let f_info = 
+    { 
+      id = f_id; 
+      gpu = false;
+      return = Int; 
+      args = [];
+      arg_names = [];
+      _blocksize = -1; 
+    }
+  in 
+  (FunctionMap.add f_id f_info fmap)
 
 let init_func_map = 
-    let time_start_fmap = 
-        let printf_fmap = 
-          add_default_func "printf" FunctionMap.empty 
-        in 
-     add_default_func "time_start" printf_fmap 
+  let time_start_fmap = 
+    let printf_fmap = 
+      add_default_func "printf" FunctionMap.empty 
     in 
+    add_default_func "time_start" printf_fmap 
+  in 
   add_default_func "time_end" time_start_fmap 
   
-
-
-
 let create = 
-   {
-       var_stack = VariableMap.empty::[];
-       func_return_type_map = init_func_map; 
-       current_function = ""; (* TODO maybe this needs a better convention *)
-       on_gpu = false;
-       gfunc_list = [];
-       num_array_returns = 0;
-       var_array_sizes = VariableMap.empty;   
-   }
+  {
+    var_stack = VariableMap.empty::[];
+    func_return_type_map = init_func_map; 
+    current_function = ""; (* TODO maybe this needs a better convention *)
+    on_gpu = false;
+    gfunc_list = [];
+    num_array_returns = 0;
+    var_array_sizes = VariableMap.empty;   
+  }
 
-(* Update gives a new env record with updated values 
- * of the record 
- *) 
-
+(* Update gives a new env record with updated values of the record *)
 let update v_stack f_map curr_f gpu g_list num_arr_ret  v_a_s= 
   { 
     var_stack = v_stack; 
@@ -114,19 +90,20 @@ let update v_stack f_map curr_f gpu g_list num_arr_ret  v_a_s=
     num_array_returns = num_arr_ret;
     var_array_sizes = v_a_s; 
   }
-(* Functions that let us modify only one 
- * variable in environment at a time 
- *)
+
+(* Functions that let us modify only one variable in environment at a
+   time *)
 let update_only_scope new_scope env = 
-	update new_scope env.func_return_type_map env.current_function env.on_gpu env.gfunc_list env.num_array_returns env.var_array_sizes
+  update new_scope env.func_return_type_map env.current_function env.on_gpu env.gfunc_list env.num_array_returns env.var_array_sizes
+
 let update_only_func new_func env = 
-	update env.var_stack new_func env.current_function env.on_gpu env.gfunc_list env.num_array_returns env.var_array_sizes
+  update env.var_stack new_func env.current_function env.on_gpu env.gfunc_list env.num_array_returns env.var_array_sizes
 
 let update_curr_func new_curr_func env = 
-	update env.var_stack env.func_return_type_map new_curr_func env.on_gpu env.gfunc_list env.num_array_returns env.var_array_sizes
+  update env.var_stack env.func_return_type_map new_curr_func env.on_gpu env.gfunc_list env.num_array_returns env.var_array_sizes
 
 let update_on_gpu gpu env = 
-	update env.var_stack env.func_return_type_map env.current_function gpu env.gfunc_list env.num_array_returns env.var_array_sizes
+  update env.var_stack env.func_return_type_map env.current_function gpu env.gfunc_list env.num_array_returns env.var_array_sizes
 
 let update_gfunc_list g_list env = 
   update env.var_stack env.func_return_type_map env.current_function env.on_gpu g_list env.num_array_returns env.var_array_sizes
@@ -135,8 +112,7 @@ let increment_num_array_rets env =
   update env.var_stack env.func_return_type_map env.current_function env.on_gpu env.gfunc_list (env.num_array_returns + 1) env.var_array_sizes
 
 let add_array_size id size env = 
-     update env.var_stack env.func_return_type_map env.current_function env.on_gpu env.gfunc_list env.num_array_returns  (VariableMap.add id size env.var_array_sizes)
-
+  update env.var_stack env.func_return_type_map env.current_function env.on_gpu env.gfunc_list env.num_array_returns  (VariableMap.add id size env.var_array_sizes)
 
 (* Checks all scopes to see if variable has been declared *)
 let is_var_in_scope id env = 
@@ -155,41 +131,43 @@ let is_var_in_scope id env =
   in check_scopes env.var_stack 
 
 (* Checks all scopes to find variable and returns type if found
- * Raises exception if not found
- *)
+ * Raises exception if not found *)
 let typeof id env = 
-   let rec check_scopes scope_stack = 
-        match scope_stack with 
-           | [] -> raise (VariableNotFound id) 
-           | scope_level :: other_scope_levels ->
-              if VariableMap.mem id scope_level then 
-                 VariableMap.find id scope_level 
-              else 
-                 check_scopes other_scope_levels 
-       in check_scopes env.var_stack
+  let rec check_scopes scope_stack = 
+    match scope_stack with 
+    | [] -> raise (VariableNotFound id) 
+    | scope_level :: other_scope_levels ->
+       if VariableMap.mem id scope_level then 
+         VariableMap.find id scope_level 
+       else 
+         check_scopes other_scope_levels 
+  in check_scopes env.var_stack
 
-(* Adds a variable to the topmost level of the variable stack
- * Does not check if variable is already in stack do elseware 
- * raises error if stack is an empty list 
- * returns an updated environment with an updated scope stack  
- *)
+(* Adds a variable to the topmost level of the variable stack. Does
+   not check if variable is already in stack do elseware raises error
+   if stack is an empty list returns an updated environment with an
+   updated scope stack *)
 let update_scope_add_var id datatype env = 
-    let old_scope, scope_tail = 
-        ( match env.var_stack with 
-            | old_scope :: scope_tail -> old_scope, scope_tail
-            | [] -> raise EmptyEnvironmentError ) in 
-    let new_scope = VariableMap.add id datatype old_scope in
-       update_only_scope (new_scope::scope_tail) env    
-    
-(* Handles adding a variable to the current environment 
- * Takes an id : string , a datatype , and a 
- * (text: string, env : env) tuple and either raises an 
- * Empty error, a AlreadyDeclared error 
- * or it adds the variable to the current top scope 
- * with update_scope_add_var 
- * and returns a updated env 
- *)
+  let old_scope, scope_tail = 
+    ( match env.var_stack with 
+      | old_scope :: scope_tail -> old_scope, scope_tail
+      | [] -> raise EmptyEnvironmentError ) in 
+  let new_scope = VariableMap.add id datatype old_scope in
+  update_only_scope (new_scope::scope_tail) env    
+		    
+(* Handles adding a variable to the current environment. 
+
+   Takes a tuple of
+   - id : string 
+   - datatype 
+   - (text: string, env : env)
   
+   and either raises 
+   - EmptyError
+   - AlreadyDeclaredError
+
+   or it adds the variable to the current top scope with
+   update_scope_add_var and returns a updated env *)
 let add_var id datatype env = 
     match env.var_stack with 
       | [] -> raise EmptyEnvironmentError 
@@ -199,101 +177,59 @@ let add_var id datatype env =
 		else 
 		update_scope_add_var id datatype env
 
-
 let get_arr_size id env = 
     if VariableMap.mem id env.var_array_sizes then 
       (VariableMap.find id env.var_array_sizes)
     else 
      raise VariableAlreadyDeclared 
 
-(* adds a new empty variableMap to the top of var stack 
- * used to enter a subscope 
- * takes an env, returns an updated env 
- *)
-    
+(* adds a new empty variableMap to the top of var stack used to enter
+   a subscope takes an env, returns an updated env *)
 let push_scope env =  
     update_only_scope (VariableMap.empty::env.var_stack) env
-;;
-(* removes a variableMap from  the top of var stack 
- * used to enter a subscope 
- * takes an env, returns an updated env 
- *)
-    
+
+(* removes a variableMap from the top of var stack used to enter a
+   subscope takes an env, returns an updated env *)
 let pop_scope env = 
    match env.var_stack with 
     | popped_scope::other_scopes -> 
         update_only_scope other_scopes env
     | [] -> raise EmptyEnvironmentError 
 
-
-(* The following methods deal with handling the function map *) 
+(* --------------------------------------------------------- *)
+(* The following methods deal with handling the function map *)
+(* --------------------------------------------------------- *)
 
 (* Takes a function id and checks if the function is defined
- * returns bool 
- *) 
+   returns bool *) 
 let is_func_declared id env = 
    FunctionMap.mem id env.func_return_type_map 
-(* Takes a function id and either returns the mapped function info * or raises an undefined function error 
- *)
+
+(* Takes a function id and either returns the mapped function info 
+   or raises an undefined function error *)
 let get_func_info id env = 
-    if is_func_declared id env then 
-        FunctionMap.find id env.func_return_type_map
-    else 
-        raise FunctionNotDefinedError
+  if is_func_declared id env then 
+    FunctionMap.find id env.func_return_type_map
+  else 
+    raise FunctionNotDefinedError
 
 let get_func_args id env =
     (get_func_info id env).args
 
-
-(* Inserts a new function to the function map 
- * and updates environment 
- * or raises a AlreadyDeclared error 
- *)
+(* Inserts a new function to the function map and updates environment
+   or raises a AlreadyDeclaredError *)
 let add_func id finfo env = 
-   if is_func_declared id env then 
-	raise VariableAlreadyDeclared
-   else
-     (* XXX hack to convert sheets main to snuggle *)
-    (* match id with
-       "main" ->
-       let env_with_snuggle =
-	 update_only_func (FunctionMap.add "snuggle" finfo env.func_return_type_map) env
-       in update_only_func (FunctionMap.add "main" finfo env.func_return_type_map) env
-     | _ ->*)
-	update_only_func (FunctionMap.add id finfo env.func_return_type_map) env	   
+  if is_func_declared id env then 
+    raise VariableAlreadyDeclared
+  else
+    update_only_func (FunctionMap.add id finfo env.func_return_type_map) env	   
        
 (* Returns the datatype of a function or 
- * raises a undefined error if the function is not defined 
- *) 
+   raises a undefined error if the function is not defined *) 
 let return_typeof_func id env = 
     let f_info = 
 	get_func_info id env in 
     f_info.return     
-
-(* The append function - this is the 
- * running loop of the codegen step 
- * First, f is a function that takes a 
- * (text, environment) tuple and matches it 
- * with a component which is either 
- * some new text - gets appended to existing text
- * some modified environment - replaces existing 
- * environment 
- * some function named gen applied to 
- * the same scope - in which case gen is applied to 
- * the existing environment and gen returns a tuple 
- * of (string, env) and the text gets appended to the 
- * existing text, and the new env replaces the old env 
- * note that gen can be a function also with arguments 
- * given to it
- * then this function f is applied to component in the 
- * list of components always returning an updated 
- * (text, env) tuple that is passed to the next 
- * component in the list 
- *) 
-(* Appends a new func_info for a gfunc to env's 
- * gdunc list and updates environment 
- * also makes sure it is a gfunc 
- *)
 
 let id_is_gfunc id env =
   let rec name_in_gfunc_list gfunc_list =
@@ -310,6 +246,7 @@ let rec check_gfunc_name_in_list glist gfunc_fdecl =
     [] -> false 
   | gfunc_fdecl :: rest_of_gfuncs -> if gfunc_fdecl.fname = gfunc_fdecl.fname then true
                                      else (check_gfunc_name_in_list rest_of_gfuncs gfunc_fdecl) 
+
 let is_gfunc_declared gfunc_fdecl env = 
   if check_gfunc_name_in_list env.gfunc_list gfunc_fdecl then 
     raise (AlreadyDeclaredError) 
@@ -318,7 +255,6 @@ let is_gfunc_declared gfunc_fdecl env =
   else false   
 
 let add_gfunc gfunc_fdecl env = 
-  (* TODO decide whether I want to check gfunc *)
   if (is_gfunc_declared gfunc_fdecl env) then 
     raise (AlreadyDeclaredError)
   else if (gfunc_fdecl.fname = "main") then
@@ -334,11 +270,24 @@ let quote_and_strip_newline str =
     match last_char str with
       '\n' -> (String.sub str 0 (len - 2))
     | _    -> str
-    
-		      
+
+(* This is the running loop of the codegen step. First, f is a
+   function that takes a (text, environment) tuple and matches it with
+   a component which is either 
+
+   * some new text - gets appended to existing text
+   * some modified environment - replaces existing environment
+   * some function named gen applied to the same scope
+
+   gen is applied to the existing environment and gen returns a tuple
+   of (string, env). The text gets appended to the existing text, and
+   the new env replaces the old env.
+
+   Note that gen can be a function also with arguments given to it
+   then this function f is applied to component in the list of
+   components always returning an updated (text, env) tuple that is
+   passed to the next component in the list *)
 let append init_env components =
-  (* add quotes around gpu statements so they print as c multi-line
-     strings *)
   let f (text, env) component =
     match component with 
     | Text(str) -> if env.on_gpu then
--- compiler/generator_utilities.ml
+++ compiler/generator_utilities.ml
@@ -1,4 +1,6 @@
-(* utilities to parse expressions for generate.ml *)
+(* generator_utilities.ml 
+   utilities to parse expressions for generate.ml *)
+
 open Ast;;
 open Environment;;
 open Printf;;
@@ -6,16 +8,6 @@ open Printf;;
 exception TypeError of string;;
 exception NotImplementedError of string;;  
 exception UnsupportedArrayTypeError;;
-
-(*
-let check_function_call f_call env =
-    (* Check if function has been declared *)
-    (* Check if args match what they should be *)
-    match f_call with
-    | Call(id, args) -> 
-
-;;
-*)
 
 let eval_basic_binop type1 type2 = 
     if (type1 = type2) then 
@@ -27,8 +19,6 @@ let eval_basic_binop type1 type2 =
             | _-> raise (TypeError("Types not ID, int, or float"))
     else 
         raise (TypeError("Incompatible types"))
-;;
-
 
 let eval_binop type1 type2 op = 
     match op with 
@@ -44,7 +34,6 @@ let eval_binop type1 type2 op =
     | Neq    -> (eval_basic_binop type1 type2) 
     | _->      raise (TypeError("Incompatible types"))
 
-
 let eval_array_acc array_ int_expr =
     match array_ with
         | Array ( Int) -> 
@@ -56,11 +45,8 @@ let eval_array_acc array_ int_expr =
                 | Int -> Float
                 | _-> raise (TypeError("Cannot access element in array with non-int datatype")))
         | _-> raise (TypeError("Cannot access element in non-array type"))
-;;
 
-(* Returns typeof block access 
- * TODO doesn't check expr type 
- *) 
+(* Returns typeof block access *) 
 let typeof_block_acc id env = 
     match id with 
    "start" -> Int 
@@ -68,86 +54,80 @@ let typeof_block_acc id env =
    | "out" -> (Environment.return_typeof_func env.current_function env)  
    | _-> raise (TypeError("Invalid block access"))
 let rec expr_typeof expr env = 
-    (*print_expr expr;*)
-    match expr with 
-     Literal_int(i) -> Int
-    | Literal_float(f) -> Float 
-    | Literal_int_a(i_a) -> Array( Int ) 
-    | Literal_float_a(i_a) -> Array(Float) 
-    | Id(s) -> Environment.typeof s env  
-    | Binop(exp1, op, exp2) -> (eval_binop (expr_typeof exp1 env) (expr_typeof exp2 env) op) 
-    | ArrayAcc(exp1, exp2) -> (eval_array_acc (expr_typeof exp1 env) (expr_typeof exp2 env) )
-    | Call(func_id, expr_list ) -> (typeof_func_call func_id expr_list (Environment.get_func_args func_id env) env)
-    | BlockAcc(id, _) -> (typeof_block_acc id env) 
-    | _-> raise (NotImplementedError("Undefined type of expression"))
+  match expr with 
+    Literal_int(i) -> Int
+  | Literal_float(f) -> Float 
+  | Literal_int_a(i_a) -> Array( Int ) 
+  | Literal_float_a(i_a) -> Array(Float) 
+  | Id(s) -> Environment.typeof s env  
+  | Binop(exp1, op, exp2) -> (eval_binop (expr_typeof exp1 env) (expr_typeof exp2 env) op) 
+  | ArrayAcc(exp1, exp2) -> (eval_array_acc (expr_typeof exp1 env) (expr_typeof exp2 env) )
+  | Call(func_id, expr_list ) -> (typeof_func_call func_id expr_list (Environment.get_func_args func_id env) env)
+  | BlockAcc(id, _) -> (typeof_block_acc id env) 
+  | _-> raise (NotImplementedError("Undefined type of expression"))
 
 and typeof_func_call func_id expr_list arg_list env = 
-    (* First make sure that all of the arguments are valid 
-     * check all ids 
-     * then return the type of the function 
-     *)
-   let rec check_expr_list expr_list arg_list = 
-      match expr_list, arg_list with 
-       | [],[] -> Environment.return_typeof_func func_id env 
-       | expr1::other_exprs, arg1::other_args -> 
-            if((expr_typeof expr1 env) != arg1 ) then
-                raise (TypeError("Function arguments of incorrect type"))
-            else
-                check_expr_list other_exprs other_args
-       | _-> raise (TypeError("Incorrect number of function arguments"))
-    in 
-      match func_id with 
-      "printf" -> Int (* TODO this is a super hacky fix *) 
-     | _ -> check_expr_list expr_list arg_list 
-;;
+  (* First make sure that all of the arguments are valid, then check
+     all ids, then return the type of the function *)
+  let rec check_expr_list expr_list arg_list = 
+    match expr_list, arg_list with 
+    | [],[] -> Environment.return_typeof_func func_id env 
+    | expr1::other_exprs, arg1::other_args -> 
+       if((expr_typeof expr1 env) != arg1 ) then
+         raise (TypeError("Function arguments of incorrect type"))
+       else
+         check_expr_list other_exprs other_args
+    | _-> raise (TypeError("Incorrect number of function arguments"))
+  in 
+  match func_id with 
+    "printf" -> Int 
+  | _ -> check_expr_list expr_list arg_list 
 
 let str_to_type str = 
         match str with 
         "int" -> Int 
        | "float" -> Float
-       | "int[]" -> Array(Int) (* TODO Enumerate other types *)  
+       | "int[]" -> Array(Int) 
        | "float[]" -> Array(Float) 
        |  _-> raise (NotImplementedError("Unrecognized type " ^ str))
 
-let rec typecheck_stmt stmt env = true;; (* TODO *)
+let rec typecheck_stmt stmt env = true
+				    
 let rec typecheck_stmt_list stmt_list env = 
-    match stmt_list with 
+  match stmt_list with 
     [] -> true 
-    | stmt :: rest_of_stmts -> typecheck_stmt stmt env; 
-                               typecheck_stmt_list rest_of_stmts env
-
-
-;;
+  | stmt :: rest_of_stmts -> typecheck_stmt stmt env; 
+                             typecheck_stmt_list rest_of_stmts env
 let vdecl_type vdecl = 
-    str_to_type vdecl.v_type
-;;
+  str_to_type vdecl.v_type
+
 let rec vdecl_list_to_type_list vdecl_list = 
-    match vdecl_list with 
-        vdecl::rest_of_vdecls -> (vdecl_type vdecl)::(vdecl_list_to_type_list rest_of_vdecls )
-       | [] -> [] 
-;;
+  match vdecl_list with 
+    vdecl::rest_of_vdecls -> (vdecl_type vdecl)::(vdecl_list_to_type_list rest_of_vdecls )
+  | [] -> [] 
+
 let rec vdecl_list_to_string_list vdecl_list = 
     match vdecl_list with 
         vdecl::rest_of_vdecls -> (vdecl.v_name)::(vdecl_list_to_string_list rest_of_vdecls )
        | [] -> [] 
-;;
+
 let fdecl_to_func_info fdecl = 
-        { 
-            id = fdecl.fname;
-            gpu = fdecl.isGfunc; 
-            return = str_to_type fdecl.r_type; 
-            args = vdecl_list_to_type_list fdecl.formals;       
-            arg_names = vdecl_list_to_string_list fdecl.formals;       
-            _blocksize = fdecl.blocksize;
-        }
-;;
+  { 
+    id = fdecl.fname;
+    gpu = fdecl.isGfunc; 
+    return = str_to_type fdecl.r_type; 
+    args = vdecl_list_to_type_list fdecl.formals;       
+    arg_names = vdecl_list_to_string_list fdecl.formals;       
+    _blocksize = fdecl.blocksize;
+  }
+
 let arr_type_str_to_base_type = function
     "float[]" -> "double"
   | "int[]" -> "int"
   | "float[][]" -> "double"
   | "int[][]" -> "int"
   | _ -> raise UnsupportedArrayTypeError
-;;
+
 let c_type_from_str = function
     "int" -> "int"
   | "float" -> "double"
@@ -156,8 +136,8 @@ let c_type_from_str = function
   | "float[][]" -> "double **"
   | "int[][]" -> "int **"
   | _ -> raise UnsupportedArrayTypeError
-;;
+
 let is_array_type = function
     "int" | "float" -> false
     | _ -> true
-;;	     
+
